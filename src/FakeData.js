82603b831650be36eedcd03128bd156cda87b15a
Shubhank-S <shubhanksharma345@gmail.com>
2022-10-22 17:37:19 +0530

    first commit

---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+    {
+      id: 1,
+      name: "Shubhank Sharma",
+      username: "Alphabeast",
+    },
+    {
+      id: 2,
+      name: "Rohi Rawal",
+      username: "FuriousSteel",
+    },
+    {
+      id: 3,
+      name: "Prajwal Pathak",
+      username: "reddevil",
+    },
+    {
+      id: 4,
+      name: "Tanishq Wadia",
+      username: "God",
+    },
+    {
+      id: 5,
+      name: "Rudraksh Joshi",
+      username: "Bheru",
+    },
+    {
+      id: 6,
+      name: "Sandeep Thakur",
+      username: "Thakur",
+    },
+    {
+      id: 7,
+      name: "Pranit mandloi",
+      username: "Rauflala",
+    },
+    {
+      id: 8,
+      name: "Umesh Patidar",
+      username: "Youdi",
+    },
+  ];
